fd70f71de8133653d68140b5261a9d1350b5d6f5
yigit polat <yigit@u.nus.edu>
2021-06-08 22:23:17 +0800

    new constants

--- include/constants.hpp
+++ include/constants.hpp
@@ -21,6 +21,7 @@ constexpr integer_t k__equal_temparament_steps{12};
 constexpr pitch_cents_t k__cents_in_octave{k__equal_temparament_steps * 100.0};
 
 // komposto defaults, mostly temporary stuff that need to be generic-ified
+constexpr floating_point_t k__default_beats_per_minute{120.0};
 constexpr velocity_t k__default_velocity{1.0};
 constexpr Dynamics k__default_dynamics{k__default_velocity};
 constexpr integer_t k__default_head_motif_count{1};
@@ -30,6 +31,7 @@ constexpr integer_t k__default_solo_section_patterns_count{1};
 constexpr integer_t k__default_solo_section_pattern_motifs_count{12};
 constexpr integer_t k__default_outro_section_patterns_count{4};
 
+constexpr floating_point_t k__minimum_beats_in_motif{3};
 constexpr integer_t k__default_pattern_motifs_count{4};
 constexpr floating_point_t k__default_mutation_amplifier{1.25};
 constexpr probability_t k__default_widening_probability{0.25};
@@ -37,7 +39,6 @@ constexpr integer_t k__default_tuning_p_limit{11};
 constexpr floating_point_t k__default_likelihood_loss_modifier{
     k__default_tuning_p_limit * 2.};
 constexpr integer_t k__default_palette_diversity_factor{20};
-constexpr floating_point_t k__default_beat_count_factor{8};
 constexpr auto k__default_calculate_tuning_p_limit{
     [](harmonic_complexity_t harmonic_complexity) -> integer_t
     {
